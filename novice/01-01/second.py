c3ac98e27a329b338baeee4aa5bf64f299650548
nzrfrz <nzr.frz@gmail.com>
2021-04-12 13:16:23 +0000

    Add string

--- novice/01-01/second.py
+++ novice/01-01/second.py
@@ -1,1 +1,23 @@
 print(2 + 2)
+print(50 - 5 * 6)
+print((50 - 5 * 6) / 4)
+print(8 / 5)
+print(17 / 3)
+print(17 // 3) # discards the fractional part
+print(17 % 3) # Return remainder of the division
+print(5 * 3 + 2)
+print(5 ** 2) # square of 5
+print(2 ** 7)
+print(7 ** 2)
+
+width = 20
+height = 5 * 9
+print(width * height)
+
+print(4 * 3.75 - 1)
+
+tax = 12.5 / 100
+price = 100.50
+print(price * tax)
+print(price + price * tax)
+print(round(price + price * tax, 2))
---
+++ novice/01-01/string3rd.py
@@ -0,0 +1,22 @@
+print('spam eggs')
+print()
+
+print("Escape single quote:")
+print('doesn\'t')
+print()
+
+print("Use double quote:")
+print("doesn't")
+print()
+
+print('"Yes," they said.')
+print()
+
+print("\"Yes,\" they said.")
+print()
+
+print('"Isn\'t," they said.')
+print()
+
+s = 'First Line. \nSecond Line'
+print(s)
