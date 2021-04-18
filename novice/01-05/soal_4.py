f025eaf45f18d39c1b84ec22647e7dba1a40f901
nzrfrz <nzr.frz@gmail.com>
2021-04-19 03:42:38 +0700

    Last week exercise

--- novice/01-05/soal_4.py
+++ novice/01-05/soal_4.py
@@ -48,3 +48,68 @@ change_first_row = latihan_6[0][2] = 10
 change_second_row = latihan_6[1][0] = 11
 result_latihan_6 = [latihan_6[0], latihan_6[1]]
 print(result_latihan_6)
+
+print("")
+print("Latihan 7. ")
+areas = [
+    "hallway", 11.25, "kitchen", 18.0,
+    "chill zone", 20.0, "bedroom", 10.75,
+    "bathroom", 10.50, "poolhouse", 24.5,
+    "garage", 15.45
+]
+del areas[8:9]
+print(areas)
+
+print("")
+print("Latihan 8. ")
+S = [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
+print(S[0:-1:2])
+
+print("")
+print("Latihan 9. ")
+europe = {'spain': 'madrid', 'france': 'paris',
+          'germany': 'berlin', 'norway': 'oslo'}
+
+
+def getList(dict):
+    return dict.keys()
+
+
+print(getList(europe))
+
+print(europe['france'])
+
+print("")
+print("Latihan 10. ")
+europe['itali'] = 'rome'
+
+print(europe)
+print('itali' in europe)
+
+print("")
+print("Latihan 11. ")
+europe2 = {'spain': 'madrid', 'france': 'paris',
+           'germany': 'bonn', 'norway': 'oslo', 'poland': 'warsaw',
+           'australia': 'vienna'}
+
+europe2['germany'] = 'berlin'
+europe2.pop('australia')
+print(europe)
+
+print("")
+print("Latihan 12. ")
+country = {
+    'spain': {'capital': 'madrid', 'population': 46.77},
+    'france': {'capital': 'paris', 'population': 66.03},
+    'germany': {'capital': 'berlin', 'population': 80.62},
+    'norway': {'capital': 'oslo', 'population': 5.084}
+}
+
+print(country['germany']['population'])
+country['indonesia'] = {'capital': 'jakarta', 'population': 250}
+print(country)
+
+print("")
+print("Latihan 13. ")
+for x, y in country.items():
+    print(f'Ibu Kota {x} adalah', y['capital'])
--- novice/01-05/weight_converter.py
+++ novice/01-05/weight_converter.py
@@ -1,21 +1,25 @@
 import math
 
 weight = int(input('Your weight are? '))
-units = input("\nON (L)lbs Or (K)kg: ")
+
 
 def weight_converter(num):
-    while type(num) == int:
-        if units.upper() == 'L':
-            weight_on_pounds = num * 0.45
-            print(f'You are {weight_on_pounds} pounds')
-        elif units.upper() == 'K':
-            weight_on_kilos = num * 0.45
-            print(f'You weight are {weight_on_kilos} kilos')
-        else:
-            print('Only on Kilos or Pounds')
+    if type(num) == int:
+        print(type(num))
+        # units = input("\nON (L)lbs Or (K)kg: ")
+        # if units.upper() == 'L':
+        #     weight_on_pounds = num * 0.45
+        #     print(f'You are {weight_on_pounds} pounds')
+        #     break
+        # elif units.upper() == 'K':
+        #     weight_on_kilos = num * 0.45
+        #     print(f'You weight are {weight_on_kilos} kilos')
+        #     break
+        # else:
+        #     print('Only on Kilos or Pounds')
+        #     break
     else:
         print("Please input numbers")
 
 
 weight_converter(weight)
-
